07e6d51ca38307b382bac3aebc84e3bd5715a8c7
davidvanleeuwen <davz0rs@gmail.com>
2023-11-14 15:06:50 +0100

    Handle UA in socket and pass only needed info into socket and empty it

--- lib/mave_metrics_web/channels/session_channel.ex
+++ lib/mave_metrics_web/channels/session_channel.ex
@@ -24,7 +24,7 @@ defmodule MaveMetricsWeb.SessionChannel do
         {:ok, video} = Stats.find_or_create_video(source_url, identifier, params["metadata"])
 
         {:ok, session} = Stats.create_session(video, key, session_attrs)
-        {:ok, socket |> assign(:session_id, session.id) |> monitor(self())}
+        {:ok, socket |> assign(:ua, nil) |> assign(:session_id, session.id) |> monitor(self())}
 
       {:error, _reason} ->
         {:error, %{reason: "invalid key"}}
@@ -155,21 +155,21 @@ defmodule MaveMetricsWeb.SessionChannel do
   end
 
   # https://github.com/phoenixframework/phoenix/issues/3844
-  defp monitor(socket, pid) do
+  defp monitor(%{assigns: %{session_id: session_id}} = socket, pid) do
     Task.Supervisor.start_child(MaveMetrics.TaskSupervisor, fn ->
       Process.flag(:trap_exit, true)
       ref = Process.monitor(pid)
 
       receive do
         {:DOWN, ^ref, :process, _pid, _reason} ->
-          on_disconnect(socket)
+          on_disconnect(session_id)
       end
     end)
 
     socket
   end
 
-  defp on_disconnect(%{assigns: %{session_id: session_id}} = _socket) do
+  defp on_disconnect(session_id) do
     with true <- Stats.duration_not_closed?(:play, session_id),
          %{timestamp: timestamp, from: from} = duration <- Stats.get_last_duration(session_id) do
       now = DateTime.utc_now()
@@ -203,47 +203,4 @@ defmodule MaveMetricsWeb.SessionChannel do
       _ -> nil
     end
   end
-
-  defp on_disconnect(_socket), do: nil
-
-  defp session_info(%{os_family: platform, device: device, client: browser}) do
-    %{
-      browser_type: browser |> get_browser_type,
-      platform: platform |> get_platform,
-      device_type: device |> get_device
-    }
-  end
-
-  defp get_browser_type(%{name: browser})
-       when browser in ["Chrome Mobile", "Chrome Mobile iOS", "Chrome"],
-       do: :chrome
-
-  defp get_browser_type(%{name: browser})
-       when browser in ["Firefox Mobile", "Firefox Mobile iOS", "Firefox"],
-       do: :firefox
-
-  defp get_browser_type(%{name: browser})
-       when browser in ["Mobile Safari", "Safari Technology Preview", "Safari"],
-       do: :safari
-
-  defp get_browser_type(%{name: browser}) when browser in ["Opera Mobile", "Opera"], do: :opera
-  defp get_browser_type(%{name: browser}) when browser in ["Edge Mobile", "Edge"], do: :edge
-
-  defp get_browser_type(%{name: browser}) when browser in ["IE Mobile", "Internet Explorer"],
-    do: :ie
-
-  defp get_browser_type(%{name: browser}) when browser in ["Brave"], do: :brave
-  defp get_browser_type(_browser), do: :other
-
-  defp get_platform(platform) when platform in ["Windows", "Mac", "Linux", "iOS", "Android"],
-    do: platform |> String.downcase() |> String.to_atom()
-
-  defp get_platform(_platform), do: :other
-
-  defp get_device(%{type: device}) when device == "smartphone", do: :mobile
-
-  defp get_device(%{type: device}) when device in ["tablet", "desktop"],
-    do: device |> String.to_atom()
-
-  defp get_device(_device), do: :other
 end
