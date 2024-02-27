7def0df9fc4f671dc3cb87be4c524441cb5ce018
davidvanleeuwen <davz0rs@gmail.com>
2024-02-27 10:07:17 +0100

    Fix typo

--- server/priv/repo/migrations/20240227085109_add_index_for_daily_session_view.exs
+++ server/priv/repo/migrations/20240227085109_add_index_for_daily_session_view.exs
@@ -2,6 +2,6 @@ defmodule MaveMetrics.Repo.Migrations.AddIndexForDailySessionView do
   use Ecto.Migration
 
   def change do
-    excute("CREATE INDEX ON daily_session_aggregation (video_id, session_date)")
+    execute("CREATE INDEX ON daily_session_aggregation (video_id, session_date)")
   end
 end
